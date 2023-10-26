8a26d87242b261223257dd05b896a8d98d74f76b
WalaceRamosDev <contatowalace.dev@gmail.com>
2023-10-26 13:39:28 -0300

    finalizando aula de algoritmos

---
+++ Aulas/JavaS/Aula01/firstaplication.h
@@ -0,0 +1,20 @@
+<html>
+<body>
+
+<h1 id="titulo" >Titulo da pagina </h1>
+
+<p  id="paragrafo">Este e o paragrafo da minha pagina</p>
+
+<p> <a href="" class="link"> Voltar </a>
+
+</body>
+</html>
+
+<script>
+
+document.querySelector("h1").style.fontSize="50pt"
+
+document.getElementById("titulo").style.fontSize="45pt"
+document.getElementById("paragrafo").style.color = "red"
+
+</script>
