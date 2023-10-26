c3dbeb8d0827d4cfbd31de3a79fe470550cd925f
WalaceRamosDev <contatowalace.dev@gmail.com>
2023-10-26 15:16:15 -0300

    repetição

--- Aulas/JavaS/mês 02/Repeticoes/repeticao02.js
+++ Aulas/JavaS/mês 02/Repeticoes/repeticao02.js
@@ -1,5 +1,16 @@
 const bolsa = [
-    'Notebook', 'Moletom', 'Escolva de dente', 'Celular', 'Carteira', 'Carregador', 'Mouse' 
+    'Notebook', 
+    'Moletom',
+    'Escolva de dente',
+    'Celular',
+    'Carteira',
+    'Carregador',
+    'Mouse' 
 ]
 
 console.log(bolsa)
+
+for(let i = 0; i < bolsa.length; i++) {
+    console.log(bolsa [i])
+    if (bolsa[i] == 'Carteira') break
+}
