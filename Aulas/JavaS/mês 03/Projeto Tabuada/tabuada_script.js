39c189c2b2f6e0bcddc53b96efc1073db67a6875
WalaceRamosDev <contatowalace.dev@gmail.com>
2023-10-30 19:42:00 -0300

    funcionalidade

---
+++ Aulas/JavaS/mês 03/Projeto Tabuada/tabuada_script.js
@@ -0,0 +1,9 @@
+function Tabuada(){
+    var num = window.document.getElementById('num')
+    var end = document.getElementById('end')
+    var n = Number(num.value)
+    var e = Number(end.value)
+    var x = 0
+    var r = ''
+    var res = window.document.getElementById('resposta') 
+}
