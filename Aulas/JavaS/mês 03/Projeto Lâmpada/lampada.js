276f826cbcec5b6f74990f0941b0996064808d62
WalaceRamosDev <contatowalace.dev@gmail.com>
2023-10-30 21:05:40 -0300

    finalizado!

--- Aulas/JavaS/mês 03/Projeto Lâmpada/lampada.js
+++ Aulas/JavaS/mês 03/Projeto Lâmpada/lampada.js
@@ -1,5 +1,20 @@
 var lamp = window.document.getElementById('lampada')
 
+function estaQuebrada() {
+    return lamp.src.indexOf('quebrada') > -1
+}
+
 function Ligar(){
-    lamp.src = 'on.svg'
+    if (!estaQuebrada()){
+    lamp.src = 'img/on.svg'}
+}
+
+function Desligar(){
+    if (!estaQuebrada()){
+    lamp.src = 'img/off.svg'}
+}
+
+lamp.addEventListener ('click', Quebrar)
+function Quebrar(){
+    lamp.src = 'img/broken.svg'
 }
