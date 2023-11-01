251ed9ae5869d63a8fa60afa8aaf952d30b86704
WalaceRamosDev <contatowalace.dev@gmail.com>
2023-10-31 22:02:30 -0300

    js

--- Aulas/JavaS/mês 03/Projeto CountDown/script.js
+++ Aulas/JavaS/mês 03/Projeto CountDown/script.js
@@ -2,3 +2,11 @@ const dia = window.document.getElementById('dia')
 const hora = window.document.getElementById('hora')
 const minuto = window.document.getElementById('minuto')
 const segundo = window.document.getElementById('segundo')
+
+const lancamento = "19 nov 2023"
+
+function countdown(){
+    const dataLanc = new Date(lancamento)
+    const hoje = new Date()
+
+}
