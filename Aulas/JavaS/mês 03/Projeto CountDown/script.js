239b2288ace0b42b0d82eec070384534f77e7521
WalaceRamosDev <contatowalace.dev@gmail.com>
2023-10-31 21:57:25 -0300

    adicionando js

---
+++ Aulas/JavaS/mês 03/Projeto CountDown/script.js
@@ -0,0 +1,4 @@
+const dia = window.document.getElementById('dia')
+const hora = window.document.getElementById('hora')
+const minuto = window.document.getElementById('minuto')
+const segundo = window.document.getElementById('segundo')
