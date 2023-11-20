42084a66399dbb9d7a6b41005de774f16db260c0
WalaceRamosDev <contatowalace.dev@gmail.com>
2023-11-20 20:53:50 -0300

    contador

--- Aulas/JavaS/mês 03/Projeto CountDown/script.js
+++ Aulas/JavaS/mês 03/Projeto CountDown/script.js
@@ -3,15 +3,16 @@ const hora = window.document.getElementById('hora')
 const minuto = window.document.getElementById('minuto')
 const segundo = window.document.getElementById('segundo')
 
-const lancamento = "19 nov 2023"
+const lancamento = "25 dec 2023"
 
 function countdown(){
     const dataLanc = new Date(lancamento)
     const hoje = new Date()
 
-    const segundosTotal = (dataLanc - hoje)/1000;
+    const segTotal = (dataLanc - hoje)/1000;
 
     const finalDias = Math.floor (segTotal / 60 / 60 / 24); 
     const finalHoras = Math.floor (segTotal /60 / 60) %24;
-
+    const finalMinutos = Math.floor (segTotal /60 ) %60;
+    const finalSegundos = Math.floor (segTotal) %60;
 }
