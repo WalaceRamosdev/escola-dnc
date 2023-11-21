5f6c55ef48137f44b4835b87f9c68cf35b34a465
WalaceRamosDev <contatowalace.dev@gmail.com>
2023-11-20 21:36:17 -0300

    objetos

---
+++ Aulas/JavaS/mês 03/Objetos/objetos03.js
@@ -0,0 +1,18 @@
+var selecao = [
+    {
+    nome: 'Neymar',
+    funcao: 'Atacante'
+},
+{
+    nome: 'Gabriel Ramos',
+    funcao: 'Meio Campo'
+
+},
+{
+    nome: 'Julio Cesar',
+    funcao: 'Goleiro'
+}
+    
+]
+
+console.log(selecao[1].nome)
