7eaf208457627e70b0a6b04e7e885821927b92b5
WalaceRamosDev <contatowalace.dev@gmail.com>
2024-02-23 11:03:20 -0300

    async await

--- Aulas/JS/mês 03/Bootstrap/index.js
+++ Aulas/JS/mês 03/Bootstrap/index.js
@@ -1,13 +1,9 @@
-function getAddressByCep() {
+async function getAddressByCep() {
     const cep = document.getElementById('cep').value;
-    fetch()
-    .then((response) => {
-        return response.json();
-    })
-    .then((response) => {
-        return response();
-    })
-    .catch((error) => {
-        console.log(erro);
-    })
+    try {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
 }
