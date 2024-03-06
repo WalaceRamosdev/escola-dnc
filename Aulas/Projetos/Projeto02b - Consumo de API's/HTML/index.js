615752654ee35eb4e8233b71b13d6f8f43244a77
WalaceRamosDev <contatowalace.dev@gmail.com>
2024-03-06 01:35:44 -0300

    att

--- Aulas/Projetos/Projeto02b - Consumo de API's/HTML/index.js
+++ Aulas/Projetos/Projeto02b - Consumo de API's/HTML/index.js
@@ -1,12 +1,15 @@
+
 async function getAddressByCep() {
     const cep = document.getElementById('cep').value;
     try {
-        const response = await fetch(`viacep.com.br/ws/${cep}/json/`)
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         const data = await response.json()
-        console.log(data);
+        console.log(data)
+
         document.getElementById('rua').value = data.logradouro
         document.getElementById('bairro').value = data.bairro
         document.getElementById('cidade').value = data.localidade
+
     } catch(error) {
         alert(error.message);
     }
