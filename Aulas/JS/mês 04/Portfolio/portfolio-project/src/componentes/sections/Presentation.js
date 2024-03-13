11ecd40bb5a6d9d26a707eeb5132b5e4f0fd53a7
Walace Ramos <contatowalace.dev@gmail.com>
2024-03-13 01:38:27 -0300

    buttons

--- Aulas/JS/mês 04/Portfolio/portfolio-project/src/componentes/sections/Presentation.js
+++ Aulas/JS/mês 04/Portfolio/portfolio-project/src/componentes/sections/Presentation.js
@@ -1,8 +1,11 @@
 import styles from './Presentation.module.css'
+import ButtonA from '../elementos/ButtonA'
+import ButtonB from '../elementos/ButtonB'
 
 function Presentation(){
     return(
         <div className={styles.presentation} id="Presentation">
+
             <h4>
                 <strong>
                     Seja bem-vindo ao meu Portfólio
@@ -10,16 +13,17 @@ function Presentation(){
             </h4>
 
             <h1>
-                Olá, me chamo Walace Ramos!
+                Olá, sou Walace Ramos!
             </h1>
 
             <p>
                 Sou um apaixonado por tecnologia,<br/>
                 e gostaria de compartilhar através<br/>
-                do meu portfólio, um pouco sobre mim<br/>
+                do meu portfólio, um pouco sobre mim,<br/>
                 e meus mais recentes projetos<br/>
                 pessoais em programação.<br/>
             </p>
+            <ButtonA link='https://www.instagram.com/walacedev.contato/' text='Conecte-se comigo!!!'/>
         </div>
     )
 }
