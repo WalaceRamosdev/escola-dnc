343451c78066a7f4ef80df6aedee543a890e922e
Walace Ramos <contatowalace.dev@gmail.com>
2024-03-13 03:44:29 -0300

    finalizado

--- Aulas/JS/mês 04/Portfolio/portfolio-project/src/componentes/sections/Footer.js
+++ Aulas/JS/mês 04/Portfolio/portfolio-project/src/componentes/sections/Footer.js
@@ -1,9 +1,28 @@
+import styles from './Footer.module.css'
+import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
+
 function Footer(){
     return(
-        <div>
-            <h1>
-                Aqui ficará o Footer
-            </h1>
+        <div className={styles.footer}>
+            <ul>
+                <li>
+                    <a href='https://www.instagram.com/walacedev.contato/' target='blank_'><FaInstagram size={30}/>
+                    </a>
+                </li>
+
+                <li>
+                    <a href='https://github.com/WalaceRamosdev' target='blank_'><FaGithub size={30}/>
+                    </a>
+                </li>
+
+                <li>
+                    <a href='https://www.linkedin.com/in/walace-ramos-dev/' target='blank_'><FaLinkedin size={30}/>
+                    </a>
+                </li>
+            </ul>
+
+            <p>contatowalace.dev@gmail.com</p>
+            <p>Walace Ramos © 2024</p>
         </div>
     )
 }
