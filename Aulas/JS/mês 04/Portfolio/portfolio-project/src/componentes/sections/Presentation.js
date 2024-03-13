db6976a2ce1ff2ef915676d2858afaae679f880d
Walace Ramos <contatowalace.dev@gmail.com>
2024-03-12 23:19:39 -0300

    again

---
+++ Aulas/JS/mês 04/Portfolio/portfolio-project/src/componentes/sections/Presentation.js
@@ -0,0 +1,11 @@
+function Presentation(){
+    return(
+        <div>
+            <h1>
+                Aqui ficará a apresentação
+            </h1>
+        </div>
+    )
+}
+
+export default Presentation
