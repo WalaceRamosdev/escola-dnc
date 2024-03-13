0e7423d5cf9a5b79b3b518a94a851e74cb5c4083
Walace Ramos <contatowalace.dev@gmail.com>
2024-03-13 02:10:41 -0300

    skills end

--- Aulas/JS/mês 04/Portfolio/portfolio-project/src/componentes/sections/Skills.js
+++ Aulas/JS/mês 04/Portfolio/portfolio-project/src/componentes/sections/Skills.js
@@ -1,9 +1,27 @@
+import styles from './Skills.module.css'
+import js from '../../image/skills/js.svg'
+import css from '../../image/skills/css.svg'
+import html from '../../image/skills/html.svg'
+import react from '../../image/skills/react.svg'
+import ts from '../../image/skills/ts.svg'
+
 function Skills(){
     return(
-        <div id="Skills">
+        <div className={styles.skill} id="Skills">
             <h1>
-                Aqui ficará as habilidades
+                Habilidades
             </h1>
+            <p>
+                Algumas das minhas principais habilidades e conhecimentos.
+            </p>
+
+            <div>
+                <img src={js}/>
+                <img src={html}/>
+                <img src={css}/>
+                <img src={react}/>
+                <img src={ts}/>
+            </div>
         </div>
     )
 }
