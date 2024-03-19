b650b36b6a7461582e37d8f709c3fbb4efb302be
Walace Ramos <contatowalace.dev@gmail.com>
2024-03-19 20:33:34 -0300

    add

---
+++ Aulas/JS/mês 04/react-ava/react-ava/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Lista from './Componentes/Lista';
+function App() {
+  return (
+    <div className="App">
+      <Lista></Lista>
+    </div>
+  );
+}
+
+export default App;
---
+++ Aulas/JS/mês 04/react-ava/react-ava/src/Componentes/Lista.js
@@ -0,0 +1,23 @@
+function Lista(){
+
+    const Mercado = [
+        {id:1, name: 'Arroz', price: 37.50},
+        {id:2, name: 'Feijão', price: 8.99},
+        {id:3, name: 'Café', price: 17.99},
+        {id:4, name: 'Açúcar', price: 4.95}
+    ]
+
+    return (
+        <div>
+            <h1>
+                Renderização de Listas
+            </h1>
+            {Mercado.map((Mercado) => (
+                <p key={Mercado.id} >{Mercado.name}</p>
+            )
+            )}
+        </div>
+    )
+}
+
+export default Lista
