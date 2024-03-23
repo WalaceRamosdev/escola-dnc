840d03a700d321a3e955aeb4ae83e2ad7ed23532
Walace Ramos <contatowalace.dev@gmail.com>
2024-03-23 20:25:02 -0300

    implementando estado em portfolio

--- Aulas/JS/mês 04/Portfolio/portfolio-project/src/componentes/elementos/Card.js
+++ Aulas/JS/mês 04/Portfolio/portfolio-project/src/componentes/elementos/Card.js
@@ -1,14 +1,26 @@
 import styles from './Card.module.css'
 import ButtonB from './ButtonB'
+import {useState} from 'react'
 
 function Card({img, title, tech, description, repo, site}) {
+
+    const [info, setInfo] = useState(false)
+    
+    function infoOn(){
+        setInfo(true)
+    }
+
+    function infoOff(){
+        setInfo(false)
+    }
+
     return (
-        <div className={styles.card}>
-                <a href={site} target="blank_">
+        <div onMouseLeave={infoOff} className={styles.card}>
+                <a onMouseEnter={infoOn} href={site} target="blank_">
                     <img src={img}/>
                 </a>
-                
-                <section>
+                {info === true &&(
+                    <section>
                     <h3>
                         {title}
                     </h3>
@@ -21,6 +33,10 @@ function Card({img, title, tech, description, repo, site}) {
 
                     <ButtonB text='Ver respositório' link={repo}/>
                 </section>
+                )
+
+                }
+                
         </div>
     )
 }
