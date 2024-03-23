2440e5396352c04355903ed57d910bafa2432693
Walace Ramos <contatowalace.dev@gmail.com>
2024-03-22 21:28:33 -0300

    react

--- Aulas/JS/mês 04/react-ava/react-ava/src/App.js
+++ Aulas/JS/mês 04/react-ava/react-ava/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 //import Lista from './Componentes/Lista';
 import Condicionais from './Componentes/Condicionais';
 function App() {
   return (
     <div className="App">
-      <Lista></Lista>
       <Condicionais></Condicionais>
     </div>
   );
--- Aulas/JS/mês 04/react-ava/react-ava/src/Componentes/Condicionais.js
+++ Aulas/JS/mês 04/react-ava/react-ava/src/Componentes/Condicionais.js
@@ -1,6 +1,20 @@
 function Condicionais() {
+
+    const Mercado = [
+        {id:1, name: 'Arroz', price: 37.50},
+        {id:2, name: 'Feijão', price: 8.99},
+        {id:3, name: 'Café', price: 17.99},
+        {id:4, name: 'Açúcar', price: 4.95}
+    ]
+
     return(
-        <div>{
+        <div>
+            {Mercado.length >=1 ?
+            (
+                <p>Você possui itens na lista</p>
+            ):(
+                <p>Você não tem itens na sua lista</p>
+            )
             }
         </div>
     )
