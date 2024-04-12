e6c1c832f0e38e5b71634033990c88fdc886980a
Walace Ramos <contatowalace.dev@gmail.com>
2024-04-12 17:57:11 -0300

    tsc

--- Aulas/JS/mês 04/Conheçendo o TypeScript/index.js
+++ Aulas/JS/mês 04/Conheçendo o TypeScript/index.js
@@ -1,4 +1,5 @@
 "use strict";
-const nome = 'Walace';
+const nome = 'Eu tenho a melhor mulher do mundo !';
 const numero = 32;
 console.log(nome);
+console.log(numero);
--- Aulas/JS/mês 04/Conheçendo o TypeScript/index.ts
+++ Aulas/JS/mês 04/Conheçendo o TypeScript/index.ts
@@ -1,4 +1,5 @@
-const nome:string = 'Walace'
+const nome:string = 'Eu tenho a melhor mulher do mundo !'
 const numero:number = 32
 
 console.log(nome)
+console.log(numero)
