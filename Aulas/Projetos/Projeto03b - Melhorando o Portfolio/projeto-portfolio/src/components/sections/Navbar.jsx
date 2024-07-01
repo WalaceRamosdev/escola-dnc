d0ccfda8a7084a08fabed03e5e4397544484697b
Walace Ramos <contatowalace.dev@gmail.com>
2024-07-01 19:13:56 -0300

    desafio 3

--- Aulas/Projetos/Projeto03b - Melhorando o Portfolio/projeto-portfolio/src/components/sections/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-function Navbar () {
-    return (
-        <div>
-            <p>Aqui ficara a minha barra de navegação, que também irá contar com alguns itens de icones de redes sociais</p>
-        </div>
-    )
-}
-
-export default Navbar
