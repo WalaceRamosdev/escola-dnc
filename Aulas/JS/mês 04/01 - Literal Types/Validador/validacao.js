3b6ddbd89be3b220e8417b0032af6fcb346dc3f6
Walace Ramos <contatowalace.dev@gmail.com>
2024-07-05 17:57:29 -0300

    typescript

---
+++ Aulas/JS/mês 04/01 - Literal Types/Validador/validacao.js
@@ -0,0 +1,9 @@
+"use strict";
+let validador;
+validador = null;
+if (validador === 'atenticado') {
+    console.log('Você está autorizado');
+}
+else {
+    console.log('Você não possui permissão');
+}
---
+++ Aulas/JS/mês 04/01 - Literal Types/Validador/validacao.ts
@@ -0,0 +1,9 @@
+let validador: 'autenticado' | null
+
+validador = null
+
+if (validador === 'atenticado') {
+    console.log('Você está autorizado')
+} else {
+    console.log(typeof(validador))
+}
