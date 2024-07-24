4b54bb49b6a063230006994344c8bee4cb49411e
Walace Ramos <contatowalace.dev@gmail.com>
2024-07-24 15:02:17 -0300

    backend

--- Aulas/Back-end/ROTAS/backend/src/app.js
+++ Aulas/Back-end/ROTAS/backend/src/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const clientRoute = require('./routes/clientRoute')
 
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
+
+app.use('/cliente', clientRoute)
 
 module.exports = app
