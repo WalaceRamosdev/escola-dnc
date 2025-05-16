b820d4a935ffc2d90934be66f1aacda898e3b1a0
Walace Ramos <contatowalace.dev@gmail.com>
2025-05-16 18:32:59 -0300

    gerenciando arquivos

---
+++ Aulas/Back-end/Semana-1/Introdução-ao-NPM/gerenciando-arquivos/fileManager.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const path = require ('path')
+
+function createDirectory(dirPath) {
+    return new Promise((resolve, reject) => {
+        fs.mkdir(dirPath, {recursive: true}, (err) => {
+            if (err) {
+                reject(err)
+            }
+            else {
+                resolve (`Diretório '${dirPath}' foi criado com sucesso! `)
+            }
+        }) 
+    })
+}
+
+function createFile(filePath, content = '') {
+    return new Promise((resolve, reject => {
+        fs.writeFile(filePath, content, 'utf-8', (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(`Arquivo '${filePath}' foi criado com sucesso!`);
+            }
+        });
+    }));
+}
+
+module.exports = {createDirectory, createFile}
