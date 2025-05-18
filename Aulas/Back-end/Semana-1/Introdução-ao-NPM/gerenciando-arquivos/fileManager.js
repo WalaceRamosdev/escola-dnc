c4ead43ede16bc99a161ce6f15499cbd48f2beab
Walace Ramos <contatowalace.dev@gmail.com>
2025-05-18 17:28:38 -0300

    update fileManager
    finalizando fileManager

--- Aulas/Back-end/Semana-1/Introdução-ao-NPM/gerenciando-arquivos/fileManager.js
+++ Aulas/Back-end/Semana-1/Introdução-ao-NPM/gerenciando-arquivos/fileManager.js
@@ -27,4 +27,52 @@ function createFile(filePath, content = "") {
     });
 }
 
-module.exports = {createDirectory, createFile}
+function listFiles (dirPath) {
+    return new Promise((resolve, reject) => {
+        fs.readdir(dirPath, (err, files) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(files);
+            }
+        });
+    });
+}
+
+function readFile(filePath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+            if (err) {
+                reject (err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+function writeFile(filePath, content) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filePath, content, 'utf-8', (err) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve('Arquivos foi escrito com sucesso !')
+            }
+        })
+    })
+}
+
+function deleteFile(filePath) {
+    return new Promise((resolve, reject) => {
+        fs.unLink(filePath, (err) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve('Arquivos deletado com sucesso !')
+            }
+        })
+    })
+}
+
+module.exports = {createDirectory, createFile, listFiles, readFile, writeFile, deleteFile}
